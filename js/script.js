90922ea9f66bc38860e06addd1803ca97403a3c3
Ailton <ailtonc.n64@gmail.com>
2025-05-29 19:21:09 -0300

    tela de home

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Função exemplo para o botão de alerta
+document.querySelector('.alert-btn img').addEventListener('click', () => {
+    alert('Abrir menu de alerta');
+});
+
+// Função exemplo para o botão de mapa
+document.querySelector('.map-btn').addEventListener('click', () => {
+    alert('Mudando tipo de mapa...');
+});
