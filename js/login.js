81f356d50c6c958e37dc6fc02fa6a56e6934a12f
Andrelgsilva <111202716+andrelgsilva@users.noreply.github.com>
2025-06-02 15:44:20 +0000

    mudanca no mapa da home e alerta

--- js/login.js
+++ js/login.js
@@ -1,4 +1,21 @@
-document.getElementById('login-btn').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita o envio de formulário
-    window.location.href = "pages/home.html"; // Redireciona para a Home
-});
+async function login() {
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if (!email || !password) {
+        alert('Por favor, preencha todos os campos!');
+        return;  // Para a execução aqui
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+    });
+
+    if (error) {
+        alert('Erro: ' + error.message);
+    } else {
+        alert('Login bem-sucedido!');
+        document.querySelector('.btn-login').innerText = 'Logado!';
+    }
+}
